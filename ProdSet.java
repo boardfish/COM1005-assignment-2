843d40417b38e8cad46e70cc1cb9a29bcfc2fa2f
Simon Fish <si@rulue.simon.fish>
2017-04-18 11:53:16 +0100

    initial commit

---
+++ ProdSet.java
@@ -0,0 +1,20 @@
+/**
+ * ProdSet.java
+ * A set of productions
+ * 2013 version
+ */
+
+ import java.util.*;
+ import java.io.*;
+
+ public class ProdSet {
+
+ 	private ArrayList<Prodn> prods;
+
+ 	public ProdSet (ArrayList a){
+ 		prods = a;
+ 		}
+
+
+    public ArrayList<Prodn> getProds() {return prods;}
+}
